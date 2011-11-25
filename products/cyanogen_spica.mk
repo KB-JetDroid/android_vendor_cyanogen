d7cc3bc6044f63421a6dcf186ecf49e6a3607c90
Tomasz Figa <tomasz.figa@gmail.com>
2011-11-25 21:29:06 +0100

    Update version string of Spica.
    Change-Id: Ib2d0dc204f7f2940f3c820a85788afd8bed9e38e

--- products/cyanogen_spica.mk
+++ products/cyanogen_spica.mk
@@ -29,10 +29,10 @@ ifdef CYANOGEN_NIGHTLY
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-Spica
+            ro.modversion=CyanogenMod-7.2.0-Spica
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-Spica-KANG
+            ro.modversion=CyanogenMod-7.2.0-Spica-KANG
     endif
 endif
 
